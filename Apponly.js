70643f2ed910589ade4b1d842ca4a199993dfc12
Ajay-Singh-Dhakad <ajay.dhakad2020@gmail.com>
2022-11-11 20:05:35 +0530

    Apponly Page

--- Apponly.js
+++ Apponly.js
@@ -1,128 +1,315 @@
-let data =[
-{
-    img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
-    name:"Global Xiomi Mi Tv box",
-    price:"$35.29",
+// let data =[
+// {
+//     img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
+//     name:"Global Xiomi Mi Tv box",
+//     price:"$35.29",
 
 
-},
-{
-    img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
-    name:"Global Xiomi Mi Tv box",
-    price:"$36.29",
+// },
+// {
+//     img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
+//     name:"Global Xiomi Mi Tv box",
+//     price:"$36.29",
 
 
-},
-{
-    img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
-    name:"Global Xiomi Mi Tv box",
-    price:"$50.29",
+// },
+// {
+//     img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
+//     name:"Global Xiomi Mi Tv box",
+//     price:"$50.29",
 
 
-},
-{
-    img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
-    name:"Global Xiomi Mi Tv box",
-    price:"$70.29",
+// },
+// {
+//     img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
+//     name:"Global Xiomi Mi Tv box",
+//     price:"$70.29",
 
 
-},
-{
-    img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
-    name:"Global Xiomi Mi Tv box",
-    price:"$80.29",
+// },
+// {
+//     img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
+//     name:"Global Xiomi Mi Tv box",
+//     price:"$80.29",
 
 
-},
-{
-    img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
-    name:"Global Xiomi Mi Tv box",
-    price:"$100.29",
+// },
+// {
+//     img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
+//     name:"Global Xiomi Mi Tv box",
+//     price:"$100.29",
 
 
-},
-{
-    img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
-    name:"Global Xiomi Mi Tv box",
-    price:"$35.29",
+// },
+// {
+//     img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
+//     name:"Global Xiomi Mi Tv box",
+//     price:"$35.29",
 
 
-},
-{
-    img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
-    name:"Global Xiomi Mi Tv box",
-    price:"$36.29",
+// },
+// {
+//     img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
+//     name:"Global Xiomi Mi Tv box",
+//     price:"$36.29",
 
 
-},
-{
-    img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
-    name:"Global Xiomi Mi Tv box",
-    price:"$50.29",
+// },
+// {
+//     img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
+//     name:"Global Xiomi Mi Tv box",
+//     price:"$50.29",
 
 
-},
-{
-    img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
-    name:"Global Xiomi Mi Tv box",
-    price:"$70.29",
+// },
+// {
+//     img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
+//     name:"Global Xiomi Mi Tv box",
+//     price:"$70.29",
 
 
-},
-{
-    img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
-    name:"Global Xiomi Mi Tv box",
-    price:"$80.29",
+// },
+// {
+//     img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
+//     name:"Global Xiomi Mi Tv box",
+//     price:"$80.29",
 
 
-},
-{
-    img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
-    name:"Global Xiomi Mi Tv box",
-    price:"$100.29",
+// },
+// {
+//     img_url:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16469/goods_thumb_220-v1/7d9a6e744268.jpg",
+//     name:"Global Xiomi Mi Tv box",
+//     price:"$100.29",
 
 
+// }
+// ];
+
+
+
+
+
+    // paginating code
+let buttons_div=document.getElementById("buttons");
+const createbuttons = (Toatal_images,images_per_page) => {
+const button=Math.ceil(Toatal_images/images_per_page);
+for(let i=1;i<=button;i++){
+    let btn=document.createElement("button");
+    btn.id=i;
+    btn.innerText=i;
+    btn.onclick =() => {
+   getpaginatedData(i,12);
+    };
+    buttons_div.append(btn);
 }
-];
+};
 
 
- let dataArray=JSON.parse(localStorage.getItem("cart_items")||[]);
+// get paginated data function
+    const getpaginatedData = async(clicked_button,limit) => {
 
-   
+    let response=await fetch(`https://636d5e73b567eed48ac032d6.mockapi.io/user?page=${clicked_button}&limit=${limit}`);
+    let data= await response.json();
+    console.log(data);
+    append(data);
+    
+    }
+    getpaginatedData(1,12);
+
+
+
+    // get data
+
+    const getData = async() => {
+        try{
+        let response=await fetch('https://636d5e73b567eed48ac032d6.mockapi.io/user');
+        let data=await response.json();
+         //append(data);
+         createbuttons(data.length,12);
+        }catch(error){
+        console.log(error);
+        }
+        };
+        getData();
+    
+    
+
+const append = (data) => {
     let box2=document.getElementById("box2");
     box2.innerHTML=null;
     data.forEach(function(el){
     let div=document.createElement("div");
     let img_url=document.createElement("img");
     img_url.src=el.img_url;
-    let name=document.createElement("p");
-    name.innerText=el.name;
+    let title=document.createElement("p");
+   title.innerText=el.title;
     let price=document.createElement("h5");
-    price.innerText=el.price;
+    price.innerText=`Price Rs. : ${el.price}`;
+   let discount=document.createElement("h5");
+   discount.setAttribute("class","dis_div");
+   discount.innerText=`${el.discount}% OFF`;
+   let actual_price=document.createElement("h4");
+   actual_price.innerText=`Offer Price: ${(el.price- (el.price*el.discount)/100)}`;
+
     let btn=document.createElement("button");
     btn.innerText="Add to Cart";
     btn.addEventListener("click",function(){
         myFun(el);
     });
-     div.append(img_url,name,price,btn);
+     div.append(img_url,title,price,discount,actual_price,btn);
     box2.append(div);
     });
- function myFun(el){
+}
+//code for Add to cart button
+        let dataArray=JSON.parse(localStorage.getItem("cart_items"))||[];
+
+         function myFun(el){
+         
         dataArray.push(el);
+      
         let x=localStorage.setItem("cart_items",JSON.stringify(dataArray));
         console.log(x);
+     
     
     }
 
-
-
-
-
-let consumer_div=document.getElementById("consumer");
-consumer_div.onclick = () => {
-   consumer();
-};
-const consumer = () => {
-    location.href="././consumer.html";
+//filter code with buttons
+//btn1
+let filterBtn=document.getElementById("consumer");
+ filterBtn.onclick = ()=>{
+let content=filterBtn.innerText;
+   filterData(content);
 }
+ // btn2
+
+let filterBtn2=document.getElementById("btn2");
+ filterBtn2.onclick = ()=>{
+let content=filterBtn2.innerText;
+   filterData(content);
+}
+
+ //btn3
+
+let filterBtn3=document.getElementById("btn3");
+ filterBtn3.onclick = ()=>{
+let content=filterBtn3.innerText;
+   filterData(content);
+}
+
+// btn4
+let filterBtn4=document.getElementById("btn4");
+ filterBtn4.onclick = ()=>{
+let content=filterBtn4.innerText;
+   filterData(content);
+}
+// btn5
+let filterBtn5=document.getElementById("btn5");
+ filterBtn5.onclick = ()=>{
+let content=filterBtn5.innerText;
+   filterData(content);
+}
+
+// btn6
+let filterBtn6=document.getElementById("btn6");
+ filterBtn6.onclick = ()=>{
+let content=filterBtn6.innerText;
+   filterData(content);
+}
+// btn7
+
+let filterBtn7=document.getElementById("btn7");
+ filterBtn7.onclick = ()=>{
+let content=filterBtn7.innerText;
+   filterData(content);
+}
+// btn8
+let filterBtn8=document.getElementById("btn8");
+ filterBtn8.onclick = ()=>{
+let content=filterBtn8.innerText;
+   filterData(content);
+}
+
+// filter code
+const filterData=async(content)=>{
+    let arr=[];
+    try{
+       let response=await fetch('https://636d5e73b567eed48ac032d6.mockapi.io/user');
+       let data=await response.json()
+       data.forEach((el)=>{
+        if(el.category===content){
+            arr.push(el);
+        }
+       })
+      // console.log(arr)
+       append(arr);
+    }catch(error){
+         console.log(error);
+    }
+};
+
+
+// btn all (ALL)
+let btnAll=document.getElementById("all");
+ btnAll.onclick = ()=>{
+All();
+}
+const All = async() => {
+    try{
+        let response=await fetch('https://636d5e73b567eed48ac032d6.mockapi.io/user');
+        let data=await response.json();
+        append(data);
+        }catch(error){
+        console.log(error);
+        }
+};
+
+
+
+
+
+
+// price high to low
+
+
+let htol=document.getElementById("htol");
+htol.onclick= () =>{
+    Sorting();
+};
+const  Sorting =async() => {
+    try{
+        let response=await fetch('https://636d5e73b567eed48ac032d6.mockapi.io/user');
+        let data=await response.json();
+        data.sort(function(a,b){
+return b.price-a.price;
+        });
+        append(data);
+}catch(error){
+    console.log(error);
+}
+    
+};
+
+
+
+// price low to high 
+
+let ltoh=document.getElementById("ltoh");
+ltoh.onclick= () =>{
+    Sorting1();
+};
+const  Sorting1 =async() => {
+    try{
+        let response=await fetch('https://636d5e73b567eed48ac032d6.mockapi.io/user');
+        let data=await response.json();
+        data.sort(function(a,b){
+return a.price-b.price;
+        });
+        append(data);
+}catch(error){
+    console.log(error);
+}
+    
+};
+
+
 
--- cart.js
+++ cart.js
@@ -1,37 +1,103 @@
 let cart= JSON.parse(localStorage.getItem("cart_items"))||[];
-let Delete_array=JSON.parse(localStorage.getItem("deleted_array"))||[];
-let box2_div=document.getElementById("box2");
 
-cart.forEach(function(el,i){
+//total count code
 
-//   let cost=cart.reduce(function(acc,el) {
-//    return acc + Number(el.price);
-//   },0);
- // document.querySelector("h1>span").innerText=cost;
- document.querySelector("h2>span").innerText=cart.length;
-
-  let div=document.createElement("div");
-  let img_url=document.createElement("img");
-  img_url.src=el.img_url;
-  let name=document.createElement("p");
-  name.innerText=el.name;
-  let price=document.createElement("h5");
-  price.innerText=el.price;
-  let btn=document.createElement("button");
-  btn.innerText="Buy Now";
-  let btn1=document.createElement("button");
-  btn1.innerText="Remove";
-  btn1.addEventListener("click",function(event){
-    event.target.parentNode.remove();
-   Delete(i);
-  })
-div.append(img_url,name,price,btn,btn1);
-box2.append(div);
-});
-
-function Delete(el,i){
-    //Delete_array.push(el);
-   cart.splice(i,1);
-   //localStorage.setItem("deleted_array",JSON.stringify(Delete_array));
-   localStorage.setItem("cart_items",JSON.stringify(cart));
+const totalcount = () => {
+  document.querySelector("#Totalitem>span").innerText=cart.length;
 }
+totalcount(cart)
+
+
+
+
+function display(cart){
+  let totalprice = 0;
+ 
+  let box2_div=document.getElementById("box2");
+       box2_div.innerHTML = null;
+
+      cart.forEach(function(el,i){ 
+
+   
+    let div=document.createElement("div");
+
+    let img_url=document.createElement("img");
+    img_url.src=el.img_url;
+
+    let title=document.createElement("p");
+   title.innerText=el.title;
+
+    let price=document.createElement("h5");
+    price.innerText=`Price Rs:${el.price}`;
+    totalprice+=+ el.price; // total price code
+
+   let discount=document.createElement("h5");
+   discount.innerText=`Dis% Rs:${el.discount}`;
+
+     let btn=document.createElement("button");
+     btn.innerText="Buy Now";
+     btn.onclick= ()=>{
+     buynow(el,i);
+     }
+
+     let btn1=document.createElement("button");
+     btn1.innerText="Remove";
+      btn1.onclick = () => {
+        Delete(el,i)
+      }
+
+     div.append(img_url,title,price,discount,btn,btn1);
+        box2_div.append(div);
+   });
+  // console.log(totalprice);
+  let cost = document.querySelector("#Totalcost>span");
+  cost.innerText = totalprice;
+}
+
+display(cart);
+
+
+
+// Delete function
+function Delete(el,i){
+    cart.splice(i,1);
+    localStorage.setItem("cart_items",JSON.stringify(cart));
+    display(cart)
+    totalcount(cart)
+ 
+  
+}
+
+let checkArray=JSON.parse(localStorage.getItem("checkout"))||[];
+const buynow = (el,i) => {
+ window.location.href='././checkout.html';
+ cart.splice(i,1);
+localStorage.setItem("cart_items",JSON.stringify(cart));
+display(cart);
+totalcount(cart)
+checkArray.push(el);
+localStorage.setItem("checkout",JSON.stringify(checkArray));
+
+
+  //  let box2_div=document.getElementById("box2");
+  //  box2_div.innerHTML=null;
+  //  let cost = document.querySelector("#Totalcost>span");
+  //  cost.innerText =el.price;
+  //  document.querySelector("#Totalitem>span").innerText=1;
+
+  //  let div=document.createElement("div");
+  //  let imgurl=document.createElement("img");
+  //  imgurl.src=el.img_url;
+  //  let titlee=document.createElement("p");
+  // titlee.innerText=el.title;
+  //  let pricee=document.createElement("h5");
+  //  pricee.innerText=`Price Rs.:${el.price}`;
+  // let discountt=document.createElement("h5");
+  // discountt.innerText=`Dis%:${el.discount}`;
+  // div.append(imgurl,titlee,pricee,discountt);
+  //    box2_div.append(div)
+    
+
+}
+
+
