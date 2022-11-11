594e926c68f1871c83869b831096f44426ab19d9
PrashantGIT7388 <yd.pt180@gmail.com>
2022-11-11 20:06:24 +0530

    new_arrival

---
+++ new_arrival.js
@@ -0,0 +1,70 @@
+let api_key='https://636d5e73b567eed48ac032d6.mockapi.io/user';
+//get data
+const getData=async()=>{
+    try{
+       let res=await fetch(api_key);
+       let data=await res.json()
+    //    console.log(data);
+    appendData(data);
+    }catch(error){
+         console.log(error);
+    }
+}
+getData();
+
+function appendData(data){
+console.log('data:', data)
+
+    let container=document.getElementById("latest_collection");
+ container.innerHTML=null;
+    data.forEach(function(el){
+   let div=document.createElement("div");
+//    div.style.onhover="box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;";
+
+
+   let img=document.createElement("img");
+   img.src=el.img_url;
+   img.style.width="400px";
+   img.style.height="350px";
+
+   let p=document.createElement("p");
+   p.innerText=el.title;
+
+   let price_div=document.createElement("div");
+   price_div.setAttribute("id","price_cart")
+
+   let p1=document.createElement("p");
+    p1.innerText=`Rs ${el.price}`;
+    p1.setAttribute("class","rupees")
+
+    let off=document.createElement("div");
+        off.innerText=`${el.discount}% off`;
+   
+        off.style.color="white";
+        off.style.textAlign="center";
+        off.style.position="absolute";
+        off.style.height="6%";
+        off.style.width="3%";
+        off.style.backgroundColor="red";
+        off.style.position="absolute";
+        off.style.marginTop="-17.2%";
+        off.style.marginLeft="16%"
+        off.style.borderRadius="50%";
+        off.style.fontSize="150%"
+        off.style.padding="0.5%"
+    let btn=document.createElement("button");
+    btn.innerText="Add to Cart" 
+    btn.setAttribute("class","cart_button");
+
+    price_div.append(p1,btn)
+    div.append(img,off,p,price_div)
+    container.append(div);
+            
+      
+          });
+
+    
+   }
+
+
+
