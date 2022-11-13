75ababae06714fced5fd62a1adfae94871d5b9f1
shkrkumar <shekhar.rrj@gmail.com>
2022-11-13 10:49:46 +0530

    superdeal final

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,7 +5,7 @@ const getData=async()=>{
 
       let res=await fetch('https://636d5e73b567eed48ac032d6.mockapi.io/checkout');
       let data=await res.json()
-   //    console.log(data);
+      console.log(data);
       append(data);
     
    
@@ -15,8 +15,9 @@ const getData=async()=>{
    }
 }
 getData();
-let cart_total;
-     
+let count=0;
+let total_mrp=0;
+let cart_total=0;    
 
 const append = (data) =>{
    let container = document.getElementById("checkout_container")
@@ -25,6 +26,8 @@ const append = (data) =>{
    // document.body.append(container);
 
    data.forEach((el)=>{
+    count++;
+    arr.push(el);
        let div = document.createElement("div");
            div.setAttribute("class", "item");
 
@@ -38,42 +41,58 @@ const append = (data) =>{
 
            let name=document.createElement("h3");
                name.innerText=el.title;
-               name.onclick=()=>{
-                 displayDetails(el);
-               }
 
-           let price_tag= document.createElement("h4");
+           let price_tag= document.createElement("h1");
                price_tag.setAttribute("class", "final_price");
                let discounted_price=+el.price-((+el.discount)*(+el.price))/100;
                price_tag.innerText=`Rs:- ${Math.ceil(discounted_price)}`;
+               cart_total+=discounted_price;
            
            let mrp=document.createElement("h5");
                mrp.innerText=`M.R.P. Rs:- ${el.price}`;
-               cart_total+=+el.price;
+               total_mrp+=+el.price;
 
-           let flash_sale=document.createElement("p");
-             flash_sale.innerText=`Hurry! only ${el.quantity} items left in stock`;
+           let qty=document.createElement("p");
+             qty.innerText=`Hurry! only ${el.quantity} items left in stock`;
            
 
-            let price=document.createElement("h3");
-            price.innerText=`Price Rs :${el.price}`;
-            cost+=+el.price;
-
-            let btn=document.createElement("button");
-            btn.innerText="Checkout";
+            let discount=document.createElement("h4");
+            discount.innerText=`Discount :${el.discount}%`;
+            
 
             div1.append(image)
-            div2.append(title,price,btn)
+            div2.append(name,price_tag,mrp,qty,discount)
            div.append(div1,div2);
-           box3_div.append(div);
+           container.append(div);
         });
         
-     let totalprice= document.querySelector("#totalprice > span");
-     totalprice.innerText=cost;
-      totalcount(cart1);
-           }
+     let show_total=document.getElementById("total_mrp");
+      show_total.textContent=total_mrp;
 
-let arr=[];
+      let saved_amount=document.getElementById("saved_amount");
+      saved_amount.innerText=total_mrp-cart_total;
+
+      let amount_tobe_paid=document.getElementById("amountTobepaid");
+      amount_tobe_paid.innerText=cart_total;
+
+      let totalNoitem=document.getElementById("count");
+      totalNoitem.innerText=count;
+
+      console.log(arr);
+  }
+
+   
+  
+
+
+  //placing order
+
+let place_order=document.getElementById("order_place");
+place_order.onclick=()=>{
+  placeOrder();
+}
+
+
 const placeOrder=async ()=>{
 
   let res=await fetch('https://636d5e73b567eed48ac032d6.mockapi.io/checkout');
